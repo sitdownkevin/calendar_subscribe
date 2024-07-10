9dad95464bc5a19d2752890111034f08266ed5b3
XU Ke <kexu567@gmail.com>
2024-07-10 11:10:34 +0800

    update code

--- tongji_calendar.py
+++ tongji_calendar.py
@@ -1,10 +1,9 @@
 from ics import Calendar, Event
 from datetime import datetime, timedelta
-import pytz
 
 # 定义开始和结束日期
-start_date = datetime(2024, 9, 2, tzinfo=pytz.timezone('Asia/Shanghai'))
-end_date = datetime(2025, 1, 10, tzinfo=pytz.timezone('Asia/Shanghai'))
+start_date = datetime(2024, 9, 2)
+end_date = datetime(2025, 1, 10)
 
 # 创建日历对象
 calendar = Calendar()
@@ -20,12 +19,10 @@ while current_date <= end_date:
         event.name = f"Week {week_number}"
         event.begin = current_date.strftime('%Y-%m-%d')
         event.make_all_day()
-        # 设置事件时区
-        event.begin = event.begin.replace(tzinfo=pytz.timezone('Asia/Shanghai'))
         calendar.events.add(event)
         week_number += 1
     current_date += timedelta(days=1)
 
 # 保存ICS文件
-with open('./data/tongji_calendar.ics', 'w') as my_file:
+with open('./data/tongji_calendar_24_25.ics', 'w') as my_file:
     my_file.writelines(calendar)
